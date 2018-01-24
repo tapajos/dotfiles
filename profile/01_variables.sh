3c180c8d40546400fbe163fcfa428187f4ce0383
Marcos Tapajos <tapajos@gmail.com>
2018-01-23 22:44:53 -0200

    remove ruby stuff

--- profile/01_variables.sh
+++ profile/01_variables.sh
@@ -7,7 +7,7 @@ export LANG=pt_BR.UTF-8
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/Cellar/mysql55/5.5.49/bin:$PATH
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/Cellar/mysql55/5.5.49/bin:/Applications/Postgres.app/Contents/Versions/10/bin:$PATH
 export PATH=$PATH:$HOME/dotfiles/bin
 
 COLOR1="\[\033[0;36m\]"
--- profile/04_commands.sh
+++ profile/04_commands.sh
@@ -2,14 +2,6 @@ alias e="$TEXT_EDITOR"
 alias ls="ls -h"
 alias myip="curl whatismyip.akamai.com"
 
-function remove_whitespaces {
-  find * -type f \( -name '*.rb' -o -name '*.js' -o -name "*.erb" -o -name "*.haml" \) -print -exec perl -pi -e 's/[\t ]+$//g' {} \;
-}
-
-function killruby {
-  pgrep '^ruby*' | xargs kill -9
-}
-
 function mac {
   echo `openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//'`
 }

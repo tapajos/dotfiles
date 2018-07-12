b7d937b29037bb4ec27836cd43c9c22db67e40ab
Marcos Tapajos <tapajos@gmail.com>
2018-07-12 18:11:45 -0300

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for FILE in bash_profile pryrc tm_properties gitconfig gitconfig-decisiv irbrc my.cnf rmate.rc gitignore_global
+for FILE in bash_profile pryrc tm_properties gitconfig gitconfig-decisiv irbrc gitignore_global
 do
   rm -Rf "$HOME/.$FILE"
   ln -s "$(pwd)/$FILE" "$HOME/.$FILE"
--- profile/08_docker.sh
+++ profile/08_docker.sh
@@ -2,10 +2,6 @@ function docker-ruby {
   docker run --rm -it -w='/data' -v $(pwd):/data ruby:latest bash
 }
 
-function docker-go {
-  docker run --rm -it -w='/go' -v $(pwd):/go golang:latest bash
-}
-
 function devenv {
   docker-compose --file $HOME/dotfiles/docker-compose.yml $@
 }

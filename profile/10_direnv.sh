317a7a0a735253b2d2051060e53158d2a3c3ddc9
Marcos Tapajos <tapajos@gmail.com>
2019-04-15 12:00:51 -0300

    Add direnv

---
+++ profile/10_direnv.sh
@@ -0,0 +1,8 @@
+_direnv_hook() {
+  local previous_exit_status=$?;
+  eval "$("/usr/local/bin/direnv" export bash)";
+  return $previous_exit_status;
+};
+if ! [[ "${PROMPT_COMMAND:-}" =~ _direnv_hook ]]; then
+  PROMPT_COMMAND="_direnv_hook${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
+fi

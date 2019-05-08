6e647beb978d4663d5e42bf39d8ab3cb1138f9e3
Marcos Tapajos <tapajos@gmail.com>
2019-05-08 16:38:28 -0300

    update

--- profile/08_docker.sh
+++ profile/08_docker.sh
@@ -5,3 +5,8 @@ function docker-ruby {
 function devenv {
   docker-compose --file $HOME/dotfiles/docker-compose.yml $@
 }
+
+function docker-clenaup {
+  docker rm -v $(docker ps -a -q)  &> /dev/null
+  docker rmi $(docker images -q) &> /dev/null
+}
